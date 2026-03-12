1ae05ea9d7c1582ed701f0fe6bdc60314ed73e86
jalil <jalil@1001.digital>
2026-03-12 17:48:45 +0100

    v1.1.0

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-siwe-auth
 # about: Authenticate users via the Sign In with Ethereum (SIWE) standard
-# version: 1.0.3
+# version: 1.1.0
 # authors: EthID
 # url: https://siwe.xyz
 
