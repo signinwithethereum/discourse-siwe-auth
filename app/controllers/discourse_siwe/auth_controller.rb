86f8c5d8636703d263d29c0699a1e2ead2836428
jalil <jalil@1001.digital>
2026-03-31 01:04:30 +0200

    Improve + harden message generation

--- app/controllers/discourse_siwe/auth_controller.rb
+++ app/controllers/discourse_siwe/auth_controller.rb
@@ -3,7 +3,7 @@
 require 'siwe'
 module DiscourseSiwe
   class AuthController < ::ApplicationController
-    skip_before_action :check_xhr, only: %i[index message]
+    skip_before_action :check_xhr, only: %i[index]
     skip_before_action :redirect_to_login_if_required, only: %i[index message]
 
     def index
@@ -12,13 +12,23 @@ module DiscourseSiwe
 
     def message
       eth_account = params[:eth_account]
-      domain = Discourse.base_url
-      domain.slice!("#{Discourse.base_protocol}://")
+      chain_id = params[:chain_id]
+
+      unless eth_account.present? && eth_account.match?(/\A0x[0-9a-fA-F]{40}\z/)
+        return render json: { error: "Invalid Ethereum address" }, status: 400
+      end
+
+      unless chain_id.present? && chain_id.match?(/\A[1-9][0-9]*\z/)
+        return render json: { error: "Invalid chain ID" }, status: 400
+      end
+
+      domain = Discourse.base_url.delete_prefix("#{Discourse.base_protocol}://")
       message = Siwe::Message.new(domain, eth_account, Discourse.base_url, "1", {
         issued_at: Time.now.utc.iso8601,
+        expiration_time: (Time.now.utc + 300).iso8601,
         statement: SiteSetting.siwe_statement,
         nonce: Siwe::Util.generate_nonce,
-        chain_id: params[:chain_id],
+        chain_id: chain_id,
       })
       session[:nonce] = message.nonce
 
